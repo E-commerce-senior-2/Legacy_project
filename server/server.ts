013b09f1ab1930394a328d5c85db4d1f51a36c58
Mohamed <hasanbouhlel@gmail.com>
2023-12-19 16:28:35 +0100

    items backend all controllers functions added

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import userRoute from "./routes/user_route";
+import itemsRoute from "./routes/item_route"
 
 const app: Application = express();
 
@@ -9,6 +10,9 @@ app.use(cors());
 
 
 app.use("/api/user", userRoute);
+
+//Items route//
+app.use("/Items" , itemsRoute )
 
 app.listen(3000, () => {
   console.log("listening on http://localhost:3000");
