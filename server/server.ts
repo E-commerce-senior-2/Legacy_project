d789933ca89984921d8c2c151f82540e317d193a
aziza0412 <azizabenhalima@live.fr>
2023-12-19 17:21:40 +0100

    following brand

--- server/server.ts
+++ server/server.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import userRoute from "./routes/user_route";
 import brandRoute from './routes/brand_route';
 import favoriteRoute from './routes/favorite_route';
-
+import followingBrandRoute from './routes/followingBrand_route'
 
 const app: Application = express();
 
@@ -16,6 +16,8 @@ app.use("/api/user", userRoute);
 app.use("/brands", brandRoute); 
 //favorite Items route:
 app.use("/favoriteItem", favoriteRoute); 
+// following  brand 
+app.use("/followingBrand", followingBrandRoute)
 
 app.listen(3000, () => {
   console.log("listening on http://localhost:3000");
