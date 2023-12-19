dec917986bdb0aeb415c870e73a8e9cad9b4dab3
manar <manar.bouaouina@gmail.com>
2023-12-19 16:12:45 +0100

    creators done

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import userRoute from "./routes/user_route";
+import creatorRoute from "./routes/creator_route";
 
 const app: Application = express();
 
@@ -9,6 +10,11 @@ app.use(cors());
 
 
 app.use("/api/user", userRoute);
+
+// creator Route:
+app.use("/creators", creatorRoute)
+
+
 
 app.listen(3000, () => {
   console.log("listening on http://localhost:3000");
