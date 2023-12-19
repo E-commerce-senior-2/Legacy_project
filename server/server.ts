edf04135c40b0c60dcb7f7402a250be64b0c5981
Mohamed <hasanbouhlel@gmail.com>
2023-12-19 16:58:27 +0100

    post backend controller functions added

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import userRoute from "./routes/user_route";
 import itemsRoute from "./routes/item_route"
+import postRoute from "./routes/post_route"
 
 const app: Application = express();
 
@@ -13,6 +14,10 @@ app.use("/api/user", userRoute);
 
 //Items route//
 app.use("/Items" , itemsRoute )
+
+
+//Posts route //
+app.use("/Posts" ,postRoute )
 
 app.listen(3000, () => {
   console.log("listening on http://localhost:3000");
