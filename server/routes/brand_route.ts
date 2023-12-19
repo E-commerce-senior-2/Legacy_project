f0b3c9358ddbf4697d04fb0a406b7f7591c463bc
aziza0412 <azizabenhalima@live.fr>
2023-12-19 14:38:49 +0100

    brand

---
+++ server/routes/brand_route.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import * as UserController from '../controllers/brand_controller';
+
+ const route=Router()
+
+
+route.get("/", UserController.getAll);
+route.get("/:brand", UserController.getOne);
+route.post("/addBrand",UserController.addBrand);
+route.put("/newStatus/:id_brand", UserController.updateStatus);
+ export default route
