b03b308becec0d6f771b06986d374c8e42d74b6c
manar <manar.bouaouina@gmail.com>
2023-12-19 18:40:53 +0100

    following creator && basket

--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,8 @@ import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import userRoute from "./routes/user_route";
 import creatorRoute from "./routes/creator_route";
-
+import followingCreator from "./routes/followingCreator_route";
+import basket from "./routes/basket_route";
 const app: Application = express();
 
 app.use(express.json());
@@ -14,7 +15,11 @@ app.use("/api/user", userRoute);
 // creator Route:
 app.use("/creators", creatorRoute)
 
+// following Creator:
+app.use("/followingCreator", followingCreator)
 
+// basket :
+app.use("/baskets", basket)
 
 app.listen(3000, () => {
   console.log("listening on http://localhost:3000");
