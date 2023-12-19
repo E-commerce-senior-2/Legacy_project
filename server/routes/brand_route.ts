fbb33c094bb392ab629e17799a1f7360c99bcbd4
aziza0412 <azizabenhalima@live.fr>
2023-12-19 16:09:17 +0100

    favorite completed

--- server/routes/brand_route.ts
+++ server/routes/brand_route.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import * as UserController from '../controllers/brand_controller';
 
- const route=Router()
+ const route = Router()
 
 
 route.get("/", UserController.getAll);
 route.get("/:brand", UserController.getOne);
 route.post("/addBrand",UserController.addBrand);
 route.put("/newStatus/:id_brand", UserController.updateStatus);
- export default route
+ 
+
+export default route
--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,9 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import userRoute from "./routes/user_route";
+import brandRoute from './routes/brand_route';
+import favoriteRoute from './routes/favorite_route';
+
 
 const app: Application = express();
 
@@ -9,6 +12,10 @@ app.use(cors());
 
 
 app.use("/api/user", userRoute);
+// brand route:
+app.use("/brands", brandRoute); 
+//favorite Items route:
+app.use("/favoriteItem", favoriteRoute); 
 
 app.listen(3000, () => {
   console.log("listening on http://localhost:3000");
