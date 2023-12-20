edbcd527a1ba8dc3c5c182e09b005bb8a0a71b99
Bilelbourgou <bilelbourgou@gmail.com>
2023-12-20 11:43:05 +0100

    fix items controller

--- server/controllers/item_controller.ts
+++ server/controllers/item_controller.ts
@@ -21,12 +21,10 @@ const CreateItem = async (req: Request, res: Response) => {
     category,
     description,
     stock,
-    collectiondId,
+    collectionId,
   } = req.body;
   try {
-    // if(!(name && status && gender && image && price && category && description && stock && collectiondId)){
-    //    return  res.status(401).send("All inputs are required!")
-    // }
+ 
     const item = await prisma.item.create({
       data: {
         name,
@@ -37,7 +35,7 @@ const CreateItem = async (req: Request, res: Response) => {
         category,
         description,
         stock,
-        collectionId: collectiondId,
+        collectionId: collectionId,
       },
     });
     res.status(201).json(item);
@@ -46,41 +44,21 @@ const CreateItem = async (req: Request, res: Response) => {
   }
 };
 
-    const getAllItems = async (req : Request , res : Response) => {
-        try {
-            const items = await prisma.item.findMany()
-            console.log(items);
-            
-            res.status(200).json(items)
-        }catch (err) {
-            res.status(400).json({error : err})
-        }
+const getBrandItems = async (req: Request, res: Response) => {
+  let { category } = req.params;
+  try {
+    const result = await prisma.item.findMany({
+      where: { category },
+    });
+    if (!!result.length) {
+      res.status(200).json(result);
+    } else {
+      res.status(230).send([]);
     }
-   const CreateItem = async (req : Request , res : Response) => {
-        let {name , status , gender , image , price , category , description , stock , collectionId} = req.body
-        console.log(req.body.collectionId);
-        
-        try {
-           const item = await prisma.item.create({
-            data :{
-                name,
-                status,
-                gender,
-                image,
-                price,
-                category,
-                description,
-                stock,
-                collectionId : collectionId,
-            }
-            })
-            res.status(201).json(item)
-        }catch (err) {
-            console.log(err);
-            
-            res.status(401).json({error : err})
-        }
-    };
+  } catch (err) {
+    res.status(400).send(err);
+  }
+};
 
 const getItemStatus = async (req: Request, res: Response) => {
   const { status } = req.params;
