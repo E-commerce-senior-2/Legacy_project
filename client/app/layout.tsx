224748d594dfabb16fcacf6d9442b8c16efa92dd
Bilelbourgou <bilelbourgou@gmail.com>
2023-12-20 15:04:48 +0100

    cerate react query provider and layout

---
+++ client/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Providers from "./provider";
+import "./globals.css";
+import Footer from "./components/footer";
+import Header from "./components/header";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "FancyMama",
+  description:
+    "Developed By El Rojlaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Header />
+        <Providers>{children}</Providers>
+        <Footer />
+      </body>
+    </html>
+  );
+}
